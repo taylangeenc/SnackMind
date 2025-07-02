c22fd67cec41b7aae851d59f7f6b7ee438f2f0ad
taylangeenc <taylanfb0534@hotmail.com>
2025-07-02 21:41:43 +0300

    Crete #Service Managers

---
+++ SnackMindService/Service/ServiceBaseClass/Lookup/State.cs
@@ -0,0 +1,37 @@
+﻿using SnackMindData.UnitOfWork;
+
+using SnackMindModel.Lookup;
+
+using SnackMindService.Service.ServiceBase.Lookup;
+
+namespace SnackMindService.Service.ServiceBaseClass.Lookup
+{
+    public class mngOrderItemState : SmService<mdlOrderItemState>, IOrderItemStateService
+    {
+        public mngOrderItemState(IUnitOfWork unitOfWork) : base(unitOfWork, unitOfWork.OrderItemState)
+        {
+            
+        }
+    }
+    public class mngTableState : SmService<mdlTableState>, ITableStateService
+    {
+        public mngTableState(IUnitOfWork unitOfWork) : base(unitOfWork, unitOfWork.TableState)
+        {
+            
+        }
+    }
+    public class mngPaymentType : SmService<mdlPaymentType>, IPaymentTypeService
+    {
+        public mngPaymentType(IUnitOfWork unitOfWork) : base(unitOfWork, unitOfWork.PaymentType)
+        {
+            
+        }
+    }
+    public class mngReservationState : SmService<mdlReservationState>, IReservationStateService
+    {
+        public mngReservationState(IUnitOfWork unitOfWork) : base(unitOfWork, unitOfWork.ReservationState)
+        {
+            mngPaymentType mngPaymentType = new mngPaymentType();
+        }
+    }
+}
