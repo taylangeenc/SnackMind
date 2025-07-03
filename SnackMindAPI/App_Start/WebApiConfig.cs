90fe5fc386b7f997b8b6b1cd867ad80eb15d793e
taylangeenc <taylanfb0534@hotmail.com>
2025-07-03 04:35:38 +0300

    Insert #Controllers

--- SnackMindAPI/App_Start/WebApiConfig.cs
+++ SnackMindAPI/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace SnackMindAPI
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
             config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
---
+++ SnackMindAPI/Controllers/Order/Order/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using SnackMindModel.Order.Order;
+
+using SnackMindService.Service.ServiceBase.Order.Order;
+
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SnackMindAPI.Controllers.Order.Order
+{
+    [RoutePrefix("api/orders")]
+    public class OrderController : ApiController
+    {
+        // GET: Order
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        [HttpPost]
+        [Route("all")]
+        public async Task<IHttpActionResult> GetAllOrders()
+        {
+            var orders = await _orderService.GetAllAsync();
+            return Ok(orders);
+        }
+        [HttpPost]
+        [Route("create")]
+        public async Task<IHttpActionResult> CreateOrder([FromBody] mdlOrder order)
+        {
+            if (order == null)
+                return BadRequest("Order is null");
+            var result = await _orderService.AddAsync(order);
+            if (result)
+                return Ok("Order created.");
+            else
+                return InternalServerError();
+        }
+    }
+}
